af08376f11561b17baf00b87b7ca7c47507d5e8d
Dmitralt <47825098+Dmitralt@users.noreply.github.com>
2019-12-26 15:01:14 +0200

    Add files via upload

--- URLconverter/converter.js
+++ URLconverter/converter.js
@@ -5,7 +5,7 @@ function convertURL(enterURL) {
   for (let line in arr) {
     if (arr[line].indexOf("sort=") == 0) {
       let elements = arr[line].replace("sort=", "").split(",");
-      arr[line] = "sort: [";
+      arr[line] = "sort\": [";
 
       for (let i in elements) {
         if (elements[i][0] == "-") {
@@ -20,15 +20,15 @@ function convertURL(enterURL) {
     }
 
     if (arr[line].indexOf("fields=") == 0) {
-      arr[line] = "fields: [\"" + arr[line].replace("fields=", "").split(",").join("\",\"") + "\"]";
+      arr[line] = "fields\": [\"" + arr[line].replace("fields=", "").split(",").join("\",\"") + "\"]";
     }
 
     if (arr[line].indexOf("offset=") == 0) {
-      arr[line] = arr[line].replace("offset=", "offset: ");
+      arr[line] = arr[line].replace("offset=", "offset\": ");
     }
 
     if (arr[line].indexOf("limit=") == 0) {
-      arr[line] = arr[line].replace("limit=", "limit: ");
+      arr[line] = arr[line].replace("limit=", "limit\": ");
     }
 
     if (arr[line].indexOf("=in:") != -1 || arr[line].indexOf("=nin:") != -1 || arr[line].indexOf("=ne:") != -1) {
@@ -52,35 +52,40 @@ function convertURL(enterURL) {
 
         if (arr[line].indexOf(fieldname + "=in") != -1) {
           elements = arr[line].replace(fieldname + "=in:", "");
-          arr[line] = fieldname + ": { $in: [";
+          arr[line] = fieldname +"\""+ ": { \"$in\": [";
         } else {
           elements = arr[line].replace(fieldname + "=nin:", "");
-          arr[line] = fieldname + ": { $nin: [";
+          arr[line] = fieldname + "\""+": { \"$nin\": [";
         }
 
         arr[line] = arr[line] + "\"" + elements.split(",").join("\",\"") + "\"";
         arr[line] = arr[line] + "] }";
       } else if (arr[line].indexOf(fieldname + "=ne") != -1) {
-        arr[line] = arr[line].replace(fieldname + "=ne:", fieldname + ": { $ne: \"");
-        arr[line]=arr[line].replace(",","\",\"");
-        arr[line] = arr[line] + "\" }";
+        arr[line] = arr[line].replace(fieldname + "=ne:", fieldname +"\""+ ": { \"$ne\": \"");
+        arr[line]=arr[line].split(",").join("\",\"");
+        arr[line] = arr[line] + "\" ";
       }
+      //arr[line] =arr[line] +"}"
     }
-
-    if (arr[line].indexOf(",") == -1) {
-      arr[line] = arr[line].replace(">=", ":{ $gte: ");
-      arr[line] = arr[line].replace(">", ":{ $gt: ");
-      arr[line] = arr[line].replace("<=", ":{ $lte: ");
-      arr[line] = arr[line].replace("<", ":{ $lt: ");
+    arr[line] = "\""+ arr[line];
+    if (arr[line].indexOf(",") == -1) { 
+      
+      arr[line] = arr[line].replace(">=", "\":{ \"$gte\": ");
+      arr[line] = arr[line].replace(">", "\":{ \"$gt\": ");
+      arr[line] = arr[line].replace("<=", "\":{ \"$lte\": ");
+      arr[line] = arr[line].replace("<", "\":{ \"$lt\": ");
 
       if (arr[line].indexOf("$") != -1) {
         arr[line] = arr[line] + "}";
       }
 
-      arr[line] = arr[line].replace("=", ": ");
+      arr[line] = arr[line].replace("=", "\": ");
     }
+    
   }
-  return '{\n'+arr.join(',\n')+'\n}';
+   
+
+   return JSON.parse('{\n'+arr.join(',\n')+'\n}')
   //console.log(arr);
 }
 
