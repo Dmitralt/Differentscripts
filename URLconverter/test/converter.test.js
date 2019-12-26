7d695ef09bd24afeb02cb0108577d036b3a98db9
Dmitralt <47825098+Dmitralt@users.noreply.github.com>
2019-12-26 15:01:31 +0200

    Add files via upload

--- URLconverter/test/converter.test.js
+++ URLconverter/test/converter.test.js
@@ -1,76 +1,20 @@
 const Converter = require('../converter');
 
 test('start gte,lte,gt,lt', () => {
-  expect(Converter('/campaigns?gte>=0&lte<=0&gt>0&lt<0')).toBe(
-`{
-gte:{ $gte: 0},
-lte:{ $lte: 0},
-gt:{ $gt: 0},
-lt:{ $lt: 0}
-}`);
-      });
-
-      test('in nin ne', () => {
-        expect(Converter('groups=in:group1,group2&nintest=in:group1,group2&netest=ne:group1,group2')).toBe(
-`{
-groups: { $in: ["group1","group2"] },
-nintest: { $in: ["group1","group2"] },
-netest: { $ne: "group1","group2" }
-}`);
-            });     
-
-
-
-
-
-
-test('start gte,owner=22', () => {
-    expect(Converter('/campaigns?start>=2376872&owner=22')).toBe(
-`{
-start:{ $gte: 2376872},
-owner: 22
-}`);
-  });
-
-  test('allFields', () => {
-        expect(Converter('/campaigns?start>=2376872&owner=22&groups=in:group1,group2&sort=-owner,+start&fields=start,end,id&offset=10&limit=5')).toBe(
-`{
-start:{ $gte: 2376872},
-owner: 22,
-groups: { $in: ["group1","group2"] },
-sort: [["owner", "desc"],["start", "asc"]],
-fields: ["start","end","id"],
-offset: 10,
-limit: 5
-}`);});
-test('manyFields', () => {
-  expect(Converter('/campaigns?end<=2376872&start>=2376872&owner=22&groups=in:group1,group2&sort=-owner,+start&first=2&second=3&fields=start,end,id&offset=10&limit=5&nintest=in:group1,group2&netest=ne:group1,group2')).toBe(
-`{
-end:{ $lte: 2376872},
-start:{ $gte: 2376872},
-owner: 22,
-groups: { $in: ["group1","group2"] },
-sort: [["owner", "desc"],["start", "asc"]],
-first: 2,
-second: 3,
-fields: ["start","end","id"],
-offset: 10,
-limit: 5,
-nintest: { $in: ["group1","group2"] },
-netest: { $ne: "group1","group2" }
-}`);
-      });
-test('start gte,lte,gt,lt', () => {
-  expect(Converter('/campaigns?gte>=0&lte<=0&gt>0&lt<0')).toBe(
-`{
-gte:{ $gte: 0},
-lte:{ $lte: 0},
-gt:{ $gt: 0},
-lt:{ $lt: 0}
-}`);
-      });
-
-    
-
-
-  //console.log(convertURL('/campaigns?start>=2376872&owner=22&groups=in:group1,group2&sort=-owner,+start&fields=start,end,id&offset=10&limit=5'))
+  expect(Converter('/campaigns?gte>=0&lte<=0&gt>0&lt<0')).toMatchObject(JSON.parse("{\"gte\": { \"$gte\": 0 },\"lte\": { \"$lte\": 0 },\"gt\": { \"$gt\": 0 },\"lt\": { \"$lt\": 0 }}"));
+});
+test('in nin ne', () => {
+  expect(Converter('A=in:group0,group1,group2&B=nin:group0,group1,group2&C=ne:group0')).toMatchObject(JSON.parse('{ "A": { "$in": [ "group0", "group1", "group2" ] }, "B": { "$nin": [ "group0", "group1", "group2" ] },"C": { "$ne": "group0" } }'));
+});
+test('desc asc', () => {
+  expect(Converter('sort=-owner,+start')).toMatchObject(JSON.parse('{ "sort": [ [ "owner", "desc" ], [ "start", "asc" ] ] }'));
+});
+test('owner', () => {
+  expect(Converter('owner=22')).toMatchObject(JSON.parse('{ "owner": 22 }'));
+});
+test('fields', () => {
+  expect(Converter('fields=start,end,id')).toMatchObject(JSON.parse('{ "fields": ["start", "end", "id"]}'));
+});
+test('offset & limmit', () => {
+  expect(Converter('offset=10&limit=5')).toMatchObject(JSON.parse('{ "offset": 10, "limit": 5}'));
+});
